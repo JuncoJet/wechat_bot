110befd33edd3e8bfd0ea74db619b65e5095016d
JuncoJet <jijivip@126.com>
2019-03-20 14:40:22 +0800

    分词器测试程序

---
+++ pyServer/fc_test.py
@@ -0,0 +1,16 @@
+﻿import requests,json
+
+r=requests.post("https://www.sogou.com/labs/webservice/sogou_word_seg.php",
+                  {
+                      'q':u'这个是输入文字测试，你测了吗',
+                      'fmt':'js'
+                   }
+              )
+j=json.loads(r.text)
+if j['status']=='OK':
+    print j['input']
+    for i in j['result']:
+        print i[0],i[1],
+else:
+    print 'status error'
+    print j
