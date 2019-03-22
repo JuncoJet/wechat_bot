2ed30d4a3a98bb76e662e1b6553c116ca97d9aaa
JuncoJet <jijivip@126.com>
2019-03-22 17:40:22 +0800

    修正bug

--- pyServer/svrmain.py
+++ pyServer/svrmain.py
@@ -5,9 +5,14 @@ from bottle import request,response,run,get,post,template,view,static_file,redir
 @post('/fc_test')
 def fc_test():
     q=request.forms.get('q')
-    payload={'q':q,'fmt':'js'}
+    if not q:
+        return
     url="https://www.sogou.com/labs/webservice/sogou_word_seg.php"
-    r=requests.post(url,params=payload)
+    r=requests.post(url,{'q':q.decode('utf8'),'fmt':'js'})
+    print q.decode('utf8')
+    print r.text
+    if not r.text:
+        return u'没有获取到数据'
     j=json.loads(r.text)
     r=''
     if j['status']=='OK':
@@ -50,16 +55,30 @@ def do_admin():
     q=request.forms.get('q')
     a=request.forms.get('a')
     if q and a:
-        payload={'q':q,'fmt':'js'}
         url="https://www.sogou.com/labs/webservice/sogou_word_seg.php"
-        r=requests.post(url,params=payload)
+        r=requests.post(url,{'q':q.decode('utf8'),'fmt':'js'})
+        if not r.text:
+            return {'code':1,'msg':u'分词器1异常'}
         qfc=json.loads(r.text)
-        r=requests.post(url,params=payload)
+        r=requests.post(url,{'q':a.decode('utf8'),'fmt':'js'})
+        if not r.text:
+            return {'code':1,'msg':u'分词器2异常'}
         afc=json.loads(r.text)
+        h={}
         if qfc['status']=='OK':
             for i in qfc['result']:
-               pass 
-    return
+                h[i[1]]=i[0]
+                db().set("INSERT INTO `wb_ck` (`cy`, `cx`) VALUES (%s, %s)",(i[0],i[1]))
+        if afc['status']=='OK':
+            for i in afc['result']:
+                if h.has_key(i[1]):
+                    db().set("INSERT INTO `wb_ck` (`cy`, `cx`) VALUES (%s, %s)",(i[0],i[1]))
+                    s1=db().get1("SELECT `id` FROM `wb_ck` WHERE `cy` = %s LIMIT 1",h[i[1]])[0]
+                    s2=db().get1("SELECT `id` FROM `wb_ck` WHERE `cy` = %s LIMIT 1",i[0])[0]
+                    db().set("INSERT INTO `wb_gx` (`cid`, `kid`) VALUES (%s, %s)",(s1,s2))
+                    db().set("UPDATE `wb_gx` SET `qz`=`qz`+1 WHERE (`cid`=%s)",s1)
+            return {'code':0}
+    return {'code':1,'msg':u'执行操作失败!'}
 
 run(host='0.0.0.0', port=8080,debug=True)
 application=default_app()
